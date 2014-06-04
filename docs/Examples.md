5ef34c83b7f2cde6941a156b8973f740cf9d5485
Wojtek Zalewski <wojtek@neverbland.com>
2014-06-04 13:57:46 +0200

    Updated docs and get_work example

---
+++ docs/Examples.md
@@ -0,0 +1,10 @@
+# Examples
+
+There just a few example requests at the moment.
+
+All you need to do, is edit `bootstrap.php` and change:
+
+```php
+$api_endpoint = 'YOUR_API_ENDPOINT';
+$access_token = 'YOUR_ACCESS_TOKEN';
+```
--- examples/get_work.php
+++ examples/get_work.php
@@ -2,15 +2,13 @@
 
 require 'bootstrap.php';
 
-$client = new GuzzleHttp\Client();
-
-$api_endpoint = 'http://slate.slateapp.com/api/v1/work/1664';
+$client = new GuzzleHttp\Client(['base_url' => $api_endpoint]);
 
 $query_params = [
     'access_token' => $access_token,
 ];
 
-$res = $client->get($api_endpoint, ['query' => $query_params]);
+$res = $client->get('work/1664', ['query' => $query_params]);
 
 // Outputs the JSON decoded data
 var_export($res->json());
